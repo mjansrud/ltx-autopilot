f649278d3d5fece7a83afd4e8be8a15b5d1b17ed
Morten Jansrud <morten.jansrud@snokam.no>
2026-04-06 23:08:12 +0200

    Fix: greedy decoding + repetition_penalty=1.2, 600 tokens max (sampling caused gibberish)

--- pipeline/captioner.py
+++ pipeline/captioner.py
@@ -279,9 +279,8 @@ class TransformersCaptioner:
                 **inputs,
                 use_audio_in_video=use_audio,
                 return_audio=False,
-                do_sample=True,
-                temperature=0.7,
-                repetition_penalty=1.3,
+                do_sample=False,
+                repetition_penalty=1.2,
                 thinker_max_new_tokens=self.max_new_tokens,
             )
 
